27462765ecc94e902f5b54ac46447d7f36c4a0ad
Felipe Alexandre <felipealexandre@INV001575.local>
2025-07-01 22:58:26 -0300

    feat: loading themes

--- App.tsx
+++ App.tsx
@@ -1,10 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
+import {
+  Baloo2_700Bold,
+  useFonts as useFontsBaloo,
+} from "@expo-google-fonts/baloo-2";
+
+import { THEME } from "@styles/theme";
 
 export default function App() {
+  const [fontsRobotoLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+  const [fontsBalooLoaded] = useFontsBaloo({ Baloo2_700Bold });
+
+  if (!fontsRobotoLoaded && !fontsBalooLoaded) {
+    return;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>
+        Open up App.tsx to start working on your app!
+      </Text>
+      <Text style={styles.subTitle}>SubTitle with Baloo</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +34,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontFamily: THEME.FONTS.BOLD,
+  },
+  subTitle: {
+    fontFamily: THEME.FONTS.BALOO_BOLD,
   },
 });
---
+++ src/styles/theme.ts
@@ -0,0 +1,31 @@
+export const THEME = {
+  COLOR: {
+    WHITE: "#FFFFFF",
+
+    GRAY_100: "#272221",
+    GRAY_200: "#403937",
+    GRAY_300: "#574F4D",
+    GRAY_400: "#8D8686",
+    GRAY_500: "#D7D5D5",
+    GRAY_600: "#E6E5E5",
+    GRAY_700: "#EDEDED",
+    GRAY_800: "#F3F2F2",
+    GRAY_900: "#FAFAFA",
+
+    YELLOW_DARK: "#C47F17",
+    YELLOW: "#DBAC2C",
+
+    PURPLE_DARK: "#4B2995",
+    PURPLE: "#8047F8",
+    PURPLE_LIGHT: "#EBE5F9",
+
+    RED_DARK: "#C44117",
+    RED: "#E8BAAB",
+    RED_LIGHT: "#F2DFD8",
+  },
+  FONTS: {
+    REGULAR: "Roboto_400Regular",
+    BOLD: "Roboto_700Bold",
+    BALOO_BOLD: "Baloo2_700Bold",
+  },
+};
