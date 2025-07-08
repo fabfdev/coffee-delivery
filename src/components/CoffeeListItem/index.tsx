366c473a1488f7db714191e7b3335c6e4b20cf22
Felipe Alexandre <felipealexandre@INV001575.local>
2025-07-08 08:02:35 -0300

    feat: animating more caffee filter

--- src/components/CoffeeListItem/index.tsx
+++ src/components/CoffeeListItem/index.tsx
@@ -1,6 +1,7 @@
-import { TouchableOpacity } from "react-native";
+import { Pressable, TouchableOpacity } from "react-native";
 import { useEffect } from "react";
 import Animated, {
+  Easing,
   interpolateColor,
   useAnimatedStyle,
   useSharedValue,
@@ -11,8 +12,8 @@ import { THEME } from "@styles/theme";
 
 import { styles } from "./styles";
 
-const TouchableOpacityAnimated =
-  Animated.createAnimatedComponent(TouchableOpacity);
+const PressableAnimated =
+  Animated.createAnimatedComponent(Pressable);
 
 type Props = {
   index: number;
@@ -23,9 +24,11 @@ type Props = {
 
 export function CoffeeListItem({ index, item, onPress, isChecked }: Props) {
   const checked = useSharedValue(0);
+  const scale = useSharedValue(1);
 
   const animatedStyle = useAnimatedStyle(() => {
     return {
+      transform: [{ scale: scale.value }],
       backgroundColor: withTiming(
         interpolateColor(
           checked.value,
@@ -48,12 +51,22 @@ export function CoffeeListItem({ index, item, onPress, isChecked }: Props) {
     };
   });
 
+  function onPressIn() {
+    scale.value = withTiming(1.2, { easing: Easing.bounce, duration: 700 });
+  }
+
+  function onPressOut() {
+    scale.value = withTiming(1, { easing: Easing.bounce, duration: 700 });
+  }
+
   useEffect(() => {
     checked.value = isChecked ? 1 : 0;
   }, [isChecked]);
 
   return (
-    <TouchableOpacityAnimated
+    <PressableAnimated
+      onPressIn={onPressIn}
+      onPressOut={onPressOut}
       key={index}
       style={[styles.filterButton, animatedStyle]}
       onPress={onPress}
@@ -61,6 +74,6 @@ export function CoffeeListItem({ index, item, onPress, isChecked }: Props) {
       <Animated.Text style={[styles.filterTitle, titleAnimatedStyle]}>
         {item.toUpperCase()}
       </Animated.Text>
-    </TouchableOpacityAnimated>
+    </PressableAnimated>
   );
 }
